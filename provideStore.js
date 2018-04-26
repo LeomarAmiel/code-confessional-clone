5c67aedde5c2804d6cc08fc0aac16cc51692e9c8
Leomar Alejar <leomaramieldev@gmail.com>
2018-04-26 21:00:54 +0800

    Create HOC for attaching component to Provider

---
+++ provideStore.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { persistStore, persistReducer } from 'redux-persist';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import rootReducer from './src/reducers';
+
+const persistConfig = {
+	key: 'root',
+	storage
+}
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(persistReducer(persistConfig, rootReducer), applyMiddleware(sagaMiddleware));
+const persistor = persistStore(store);
+
+export function provideStore ( Component ) {
+	return class Container extends React.Component {
+		render() {
+			return (
+				<Provider store={store}>
+					<PersistGate loading={null} persistor={persistor}>
+						<Component {...this.props}/>
+					</PersistGate>
+				</Provider>
+			);
+		}
+	}
+}
--- src/screens/index.js
+++ src/screens/index.js
@@ -2,9 +2,10 @@ import { Navigation } from 'react-native-navigation';
 
 import Feed from './Feed';
 import Confess from './Confess';
+import { provideStore } from '../../provideStore';
 
 // register all screens of the app (including internal ones)
-export function registerScreens() {
-  Navigation.registerComponent('Feed', () => Feed);
-  Navigation.registerComponent('Confess', () => Confess);
+export function registerScreens(store, Provider) {
+  Navigation.registerComponent('Feed', () => provideStore(Feed));
+  Navigation.registerComponent('Confess', () => provideStore(Confess));
 }
